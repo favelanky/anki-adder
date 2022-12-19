00fe54165139e055cb4a644eacbabb89ca537abb
Shamil <favelanky@gmail.com>
2022-12-19 23:38:27 +0300

    translations adding function (to Anki)

---
+++ src/card_adder.py
@@ -0,0 +1,24 @@
+def add_translation(word, translation_tuple, synonyms):
+    """Function for adding word to Anki"""
+    translations, examples = translation_tuple
+    with open("D:\\PyCharmProjects\\anki_adder\\src\\test.txt", "w", encoding='UTF-8') as f:
+        f.write(f"<b>{word}</b> :")
+        for i in range(min(5, len(synonyms))):
+            f.write(f" {synonyms[i]}")
+            if i + 1 == min(5, len(synonyms)):
+                f.write(".")
+            else:
+                f.write(",")
+        f.write("<br>")
+        f.write("<br>")
+        color = '<b style="background-color: #ffe100; color: #181818">'
+        f.write(f'{examples[0].replace(word, color+word+"</b>")}<br>')
+        f.write(f'{examples[1].replace(word, color+word+"</b>")}')
+        f.write("~")
+        for i in range(min(5, len(translations))):
+            f.write(f" {translations[i]}")
+            if i + 1 == min(5, len(translations)):
+                f.write(".")
+            else:
+                f.write(",")
+        f.write("~")
